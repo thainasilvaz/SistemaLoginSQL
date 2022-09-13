a2c0bc1f9c87d47fc8a87cebcd89083317398c6c
Thainá Silva <thaina.silva254@etec.sp.gov.br>
2022-09-13 12:01:55 -0300

    sistema_login

---
+++ controller/ControllerCadastro.php
@@ -0,0 +1,28 @@
+<?php
+require_once("../model/cadastro.php");
+class cadastroController{
+
+    private $cadastro;
+
+    public function __construct(){
+        $this->cadastro = new Cadastro();
+        $this->incluir();
+    }
+
+    private function incluir(){
+        $this->cadastro->setEmail($_POST['txtEmail']);
+        $this->cadastro->setSenha($_POST['txtSenha']);
+        $this->cadastro->setEndereco($_POST['txtEndereco']);
+        $this->cadastro->setBairro($_POST['txtBairro']);
+        $this->cadastro->setCep($_POST['txtCep']);
+        $this->cadastro->setCidade($_POST['txtCidade']);
+        $this->cadastro->setEstado($_POST['txtEstado']);
+        $result = $this->cadastro->incluir();
+        if($result >= 1){
+            echo "<script>alert('Registro incluído com sucesso!');document.location='../index.php'</script>";
+        }else{
+            echo "<script>alert('Erro ao gravar registro!');</script>";
+        }
+    }
+}
+new cadastroController();
